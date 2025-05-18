1a773fbe56f98ea6920e2e97b606a7306882c36a
phoebeobaje <phoeberobertscareer@gmail.com>
2025-05-18 21:51:37 +0100

    alx-listing-app

---
+++ interfaces/index.ts
@@ -0,0 +1,9 @@
+
+// export interface CardProps {
+//   // Add props here later, e.g. title: string;
+// }
+
+// // Props for a Button component
+// export interface ButtonProps {
+//   // Add props here later, e.g. onClick: () => void;
+// }
